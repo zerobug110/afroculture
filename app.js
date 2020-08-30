b09115c6211ea014c9401ab9b2b94a8f2c9a6841
zerobug110 <mahabojang110@gmail.com>
2020-08-30 00:04:35 +0000

    added eventlistners to the cart section

---
+++ app.js
@@ -0,0 +1,24 @@
+// Variables
+const addToCart = document.querySelector('.order-btn-mini');
+const cartContainer = document.querySelector('.cart-icon');
+const close = document.querySelector('.cart-close');
+
+
+// EventListeners
+addToCart.addEventListener('click', () => {
+
+});
+
+cartContainer.addEventListener('click', () => {
+    document.querySelector('.cart-container').style.display = 'block'
+});
+
+close.addEventListener('click', () => {
+    document.querySelector('.cart-container').style.display = 'none'
+
+});
+
+
+
+
+// Functions
